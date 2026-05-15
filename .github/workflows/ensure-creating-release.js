b4e4f7408c79e2a525088ae9b6c453989880d8bc
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-05-15 12:32:09 +0900

    Fix style

--- .github/workflows/ensure-creating-release.js
+++ .github/workflows/ensure-creating-release.js
@@ -1,17 +1,11 @@
-const {owner: owner, repo: repo} = context.repo;
+const {owner : owner, repo : repo} = context.repo;
 const tag = context.ref.replace("refs/tags/", "")
 try {
-  const {data: release} = await github.repos.getReleaseByTag({
-    owner: owner,
-    repo: repo,
-    tag: tag
-  });
+  const {data : release} =
+    await github.repos.getReleaseByTag({owner : owner, repo : repo, tag : tag});
   return release.upload_url;
 } catch (e) {
-  const {data: createdRelease} = await github.repos.createRelease({
-    owner: owner,
-    repo: repo,
-    tag_name: tag
-  });
+  const {data : createdRelease} = await github.repos.createRelease(
+    {owner : owner, repo : repo, tag_name : tag});
   return createdRelease.upload_url;
 }
